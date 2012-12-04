16e8b8ae97b5d62ba077c2b5915de10bfccb0149
MaximilienDethier <kenzouilli@hotmail.com>
2012-12-04 17:38:41 +0100

    Premiere Integration Drupal feedback1

---
+++ themes/logopedie/js/script.js
@@ -0,0 +1,26 @@
+// (function ($) {
+
+	//Variables Globales
+	
+	// var gMap;
+	
+	//Fonction pour générer la map
+	// var generateMap = function(){
+	
+		// gMap = new google.maps.Map(document.getElementById('mapUnite'), { 
+				// zoom:16,
+				// mapTypeId: google.maps.MapTypeId.HYBRID,
+				// center: new google.maps.LatLng(50.587594,5.570228)
+		// } );	
+	// };	
+		
+	// $(function(){
+	
+		// if(document.getElementById('mapUnite'))
+		// {	
+			// generateMap();
+		// }
+
+	// });
+	
+// })(jQuery);
--- themes/logopedie/templates/html.tpl.php
+++ themes/logopedie/templates/html.tpl.php
@@ -2,6 +2,7 @@
 <html>
     <head>
     <meta charset="UTF-8" />
+	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400italic,700' rel='stylesheet' type='text/css'>
 	<?php print $styles; ?>
     <title>
         Unité Logopédie de la Voix 
@@ -9,5 +10,8 @@
     </head>
     <body>
 		<?php print $page ?>
+		
+		<script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDHJ3p-sn1Y5tJGrzH9MF5cbR5sdsDmhfg&sensor=false"></script>
+		<?php print $scripts; ?>
     </body>
 </html>
